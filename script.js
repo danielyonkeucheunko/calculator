9de4a1efd5a1c46be4a0cf9506d0d5f116b47c6c
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-05-02 00:39:48 -0400

    Finished Assignment

--- script.js
+++ script.js
@@ -1,38 +1,82 @@
-let num1;
-let num2;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
+let displayValue = "";
 
 function add(a, b) {
-    return a + b;
+    return Math.round((Number(a) + Number(b)) * 1000) / 1000;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Math.round((Number(a) - Number(b)) * 1000) / 1000;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Math.round(Number(a) * Number(b) * 1000) / 1000;
 }
 
 function divide(a, b) {
-    return a / b;
+    return Math.round((Number(a) / Number(b)) * 1000) / 1000;
 }
 
 function operate(number1, operator, number2) {
     switch (operator) {
         case "+":
-            add(number1, number2);
-            break;
+            return add(number1, number2);
         case "-":
-            subtract(number1, number2);
-            break;
+            return subtract(number1, number2);
         case "x":
-            multiply(number1, number2);
-            break;
+            return multiply(number1, number2);
         case "/":
-            divide(number1, number2);
-            break;
+            return divide(number1, number2);
         default:
             break;
     }
 }
+
+const display = document.querySelector("#display");
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", (event) => {
+        if (displayValue.length < 13) {
+            displayValue += String(event.target.textContent);
+        }
+        display.textContent = displayValue;
+    });
+});
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", () => {
+    displayValue = "";
+    num1 = "";
+    operator = "";
+    num2 = "";
+    display.textContent = displayValue;
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", (event) => {
+        num1 = displayValue;
+        displayValue = "";
+        operator = event.target.textContent;
+    });
+});
+
+const equalButton = document.querySelector("#equal");
+equalButton.addEventListener("click", () => {
+    num2 = displayValue;
+
+    if (displayValue === "") {
+        alert("Make an Expression before Evaluating!");
+    } else if (operator === "/" && num2 === "0") {
+        alert("Did you seriously try that? Really?");
+        displayValue = "";
+        num2 = "";
+    } else {
+        displayValue = "";
+        displayValue = String(operate(num1, operator, num2));
+        display.textContent = displayValue;
+    }
+});
